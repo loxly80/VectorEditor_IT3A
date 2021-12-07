10a35765bcd579bada944f692c53a650c74d3e65
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2021-12-07 14:00:05 +0100

    Před hodinou

--- VectorEditor_IT3A/Form1.Designer.cs
+++ VectorEditor_IT3A/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Canvas = new System.Windows.Forms.PictureBox();
+            this.sfd = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Canvas
+            // 
+            this.Canvas.BackColor = System.Drawing.Color.White;
+            this.Canvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Canvas.Location = new System.Drawing.Point(0, 0);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(1090, 636);
+            this.Canvas.TabIndex = 0;
+            this.Canvas.TabStop = false;
+            this.Canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.Canvas_Paint);
+            this.Canvas.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseClick);
+            // 
+            // sfd
+            // 
+            this.sfd.FileOk += new System.ComponentModel.CancelEventHandler(this.sfd_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1090, 636);
+            this.Controls.Add(this.Canvas);
+            this.KeyPreview = true;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Canvas;
+        private System.Windows.Forms.SaveFileDialog sfd;
     }
 }
 
--- VectorEditor_IT3A/Form1.cs
+++ VectorEditor_IT3A/Form1.cs
@@ -3,19 +3,56 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace VectorEditor_IT3A
 {
     public partial class Form1 : Form
     {
+        List<PointF> points;
+
         public Form1()
         {
             InitializeComponent();
+            points = new List<PointF>();
         }
 
+        private void Canvas_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Text = e.Location.ToString();
+            points.Add(e.Location);
+            Canvas.Refresh();
+        }
+
+        private void Canvas_Paint(object sender, PaintEventArgs e)
+        {
+            if(points.Count > 1)
+            {
+                e.Graphics.DrawLines(Pens.Red, points.ToArray());
+            }
+            foreach (var point in points)
+            {
+                e.Graphics.FillRectangle(Brushes.Blue, point.X - 4, point.Y - 4, 8, 8);
+            }
+        }
+
+        private void sfd_FileOk(object sender, CancelEventArgs e)
+        {
+            var json = JsonConvert.SerializeObject(points, Formatting.Indented);
+            File.WriteAllText(sfd.FileName, json);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.S && e.Control)
+            {
+                sfd.ShowDialog();
+            }
+        }
     }
 }
